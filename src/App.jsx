c250cc555314b72874c8284f062a0a8edd6bc861
Hashir Ahmad <ahashir01@gmail.com>
2025-04-09 10:24:45 -0400

    Added in more fetures in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomeVideo from "./components/HomeVideo";
 import TrendingGifts from "./components/TrendingGifts";
 import AIGiftIdeas from "./components/AIGiftIdeas";
-import HomeVideo from "./components/HomeVideo";
 import GiftCard from "./components/GiftCard";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 import "./App.css";
-
-
-
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <HomeVideo />
-      <AIGiftIdeas />
-      <GiftCard />
-      <TrendingGifts />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main>
+        <Routes>
+          <Route path="/" element={<HomeVideo />} />
+          <Route path="/ai-gift-ideas" element={<AIGiftIdeas />} />
+          <Route path="/gift-card" element={<GiftCard />} />
+          <Route path="/trending-gifts" element={<TrendingGifts />} />
+        </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
